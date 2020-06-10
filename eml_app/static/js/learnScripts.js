2a142abea47461531f95b48097e7621a3cd56329
daphna.lev <daphna.leventer@mail.huji.ac.il>
2020-06-10 23:17:50 +0300

    Add improved site w/ connection to python

---
+++ eml_app/static/js/learnScripts.js
@@ -0,0 +1,16 @@
+function stage1() {
+    document.getElementById("2").hidden = false;
+}
+
+function stage2() {
+    document.getElementById("3").hidden = false;
+}
+
+function stage3() {
+    document.getElementById("4").hidden = false;
+}
+
+function stage4() {
+    document.getElementById("5").hidden = false;
+}
+
--- eml_app/urls.py
+++ eml_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index ),
+    path('', views.index, name='index'),
+    path('learn', views.learn, name='learn'),
 ]
